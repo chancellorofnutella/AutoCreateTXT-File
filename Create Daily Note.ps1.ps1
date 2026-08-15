89c8246cd081f1eae0c29818c839734af22c2be0
chancellorofnutella <chancellorofnutella@gmail.com>
2026-08-15 14:10:06 -0700

    Add files via upload

---
+++ Create Daily Note.ps1.ps1
@@ -0,0 +1,3 @@
+﻿$archivePath = Split-Path -Parent $PSCommandPath
+$fileName = (Get-Date -Format 'MM.dd.yyyy') + '.txt'
+New-Item -ItemType File -Path (Join-Path $archivePath $fileName) -Force | Out-Null
